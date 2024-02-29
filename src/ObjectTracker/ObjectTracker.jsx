d4cd0fb26a6bd0c26cb0be8c6cce4e726eca7fe1
Rishu Anand <riuandg5@gmail.com>
2024-02-29 09:30:45 +0530

    add opencv camshift tracker

--- src/ObjectTracker/ObjectTracker.jsx
+++ src/ObjectTracker/ObjectTracker.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { useRef, useState, useEffect } from "react";
 
 import "./ObjectTracker.css";
@@ -7,6 +8,7 @@ const MARKER_WIDTH = 4;
 
 export default function ObjectTracker({ videoFile }) {
     const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+    const [isVideoEnded, setIsVideoEnded] = useState(false);
     const [videoCurrentTime, setVideoCurrentTime] = useState(0);
     const [videoDuration, setVideoDuration] = useState(0);
 
@@ -15,14 +17,24 @@ export default function ObjectTracker({ videoFile }) {
     const [markerEndPoint, setMarkerEndPoint] = useState({ x: 0, y: 0 });
     const [isValidMarker, setIsValidMarker] = useState(false);
 
+    const [isTracking, setIsTracking] = useState(false);
+
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
     const contextRef = useRef(null);
+
+    const isVideoEndedRef = useRef(isVideoEnded);
+    const isValidMarkerRef = useRef(isValidMarker);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         contextRef.current = canvas.getContext("2d");
     }, []);
+
+    useEffect(() => {
+        isVideoEndedRef.current = isVideoEnded;
+        isValidMarkerRef.current = isValidMarker;
+    }, [isVideoEnded, isValidMarker]);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -79,10 +91,19 @@ export default function ObjectTracker({ videoFile }) {
         }
     }
 
+    function handleVideoEnded() {
+        setIsVideoPlaying(false);
+        setIsVideoEnded(true);
+    }
+
     function handleVideoSeek(e) {
         const newTime = e.target.value;
         const video = videoRef.current;
         video.currentTime = newTime;
+
+        if (newTime !== videoDuration) {
+            setIsVideoEnded(false);
+        }
     }
 
     function formatTime(time) {
@@ -133,10 +154,141 @@ export default function ObjectTracker({ videoFile }) {
     function handleResetMark() {
         setMarkerEndPoint({ x: markerStartPoint.x, y: markerStartPoint.y });
         setIsValidMarker(false);
+        setIsTracking(false);
 
         const video = videoRef.current;
         video.pause();
         setIsVideoPlaying(false);
+    }
+
+    function startTracking() {
+        const video = videoRef.current;
+        const canvas = canvasRef.current;
+        const context = contextRef.current;
+        context.strokeStyle = MARKER_COLOR;
+        context.lineWidth = MARKER_WIDTH;
+
+        setIsTracking(true);
+
+        if (isVideoEnded) {
+            setIsVideoEnded(false);
+            video.currentTime = 0;
+        }
+        video.play();
+        setIsVideoPlaying(true);
+
+        const cap = new cv.VideoCapture(video);
+        const frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+        cap.read(frame);
+
+        let trackWindow = new cv.Rect(
+            Math.min(markerStartPoint.x, markerEndPoint.x),
+            Math.min(markerStartPoint.y, markerEndPoint.y),
+            Math.abs(markerStartPoint.x - markerEndPoint.x),
+            Math.abs(markerStartPoint.y - markerEndPoint.y)
+        );
+
+        const roi = frame.roi(trackWindow);
+        const hsvRoi = new cv.Mat();
+        cv.cvtColor(roi, hsvRoi, cv.COLOR_RGBA2RGB);
+        cv.cvtColor(hsvRoi, hsvRoi, cv.COLOR_RGB2HSV);
+
+        const mask = new cv.Mat();
+        const lowScalar = new cv.Scalar(30, 30, 0);
+        const highScalar = new cv.Scalar(180, 180, 180);
+        const low = new cv.Mat(
+            hsvRoi.rows,
+            hsvRoi.cols,
+            hsvRoi.type(),
+            lowScalar
+        );
+        const high = new cv.Mat(
+            hsvRoi.rows,
+            hsvRoi.cols,
+            hsvRoi.type(),
+            highScalar
+        );
+        cv.inRange(hsvRoi, low, high, mask);
+
+        const roiHist = new cv.Mat();
+        const hsvRoiVec = new cv.MatVector();
+        hsvRoiVec.push_back(hsvRoi);
+        cv.calcHist(hsvRoiVec, [0], mask, roiHist, [180], [0, 180]);
+        cv.normalize(roiHist, roiHist, 0, 255, cv.NORM_MINMAX);
+
+        roi.delete();
+        hsvRoi.delete();
+        mask.delete();
+        low.delete();
+        high.delete();
+        hsvRoiVec.delete();
+
+        const termCrit = new cv.TermCriteria(
+            cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT,
+            10,
+            1
+        );
+
+        const hsv = new cv.Mat(video.height, video.width, cv.CV_8UC3);
+        const hsvVec = new cv.MatVector();
+        hsvVec.push_back(hsv);
+        const dst = new cv.Mat();
+        let trackBox = null;
+
+        function processVideo() {
+            try {
+                if (isVideoEndedRef.current || !isValidMarkerRef.current) {
+                    setIsTracking(false);
+
+                    frame.delete();
+                    dst.delete();
+                    hsvVec.delete();
+                    roiHist.delete();
+                    hsv.delete();
+                    return;
+                }
+
+                cap.read(frame);
+
+                cv.cvtColor(frame, hsv, cv.COLOR_RGBA2RGB);
+                cv.cvtColor(hsv, hsv, cv.COLOR_RGB2HSV);
+                cv.calcBackProject(hsvVec, [0], roiHist, dst, [0, 180], 1);
+
+                [trackBox, trackWindow] = cv.CamShift(
+                    dst,
+                    trackWindow,
+                    termCrit
+                );
+
+                const pts = cv.rotatedRectPoints(trackBox);
+
+                context.clearRect(0, 0, canvas.width, canvas.height);
+
+                for (let i = 0; i <= pts.length; i++) {
+                    if (i === 0) {
+                        context.beginPath();
+                        context.moveTo(
+                            Math.round(pts[i].x),
+                            Math.round(pts[i].y)
+                        );
+                    } else if (i === pts.length) {
+                        context.closePath();
+                        context.stroke();
+                    } else {
+                        context.lineTo(
+                            Math.round(pts[i].x),
+                            Math.round(pts[i].y)
+                        );
+                    }
+                }
+
+                requestAnimationFrame(processVideo);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        requestAnimationFrame(processVideo);
     }
 
     return (
@@ -146,6 +298,8 @@ export default function ObjectTracker({ videoFile }) {
                     ref={videoRef}
                     onTimeUpdate={handleVideoTimeUpdate}
                     onLoadedMetadata={handleVideoLoadedMetadata}
+                    onEnded={handleVideoEnded}
+                    muted
                 >
                     <source src={URL.createObjectURL(videoFile)} />
                 </video>
@@ -185,6 +339,12 @@ export default function ObjectTracker({ videoFile }) {
                 {isValidMarker && (
                     <button type="button" onClick={handleResetMark}>
                         Reset Mark
+                    </button>
+                )}
+                {isValidMarker && !isTracking && (
+                    <button type="button" onClick={startTracking}>
+                        Start Tracking{" "}
+                        {isValidMarker && isVideoEnded && "Again"}
                     </button>
                 )}
             </div>
